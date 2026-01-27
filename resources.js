1560df2408ec165c836d92209fc467a12fdc282a
sree2324-lemj <palakodetisreecharan@gmail.com>
2026-01-27 12:05:57 -0500

    Update resources.js

--- resources.js
+++ resources.js
@@ -3,11 +3,12 @@ class Resources {
   #rows;
   #filterOption;
 
-  inputHtml() {
-    this.#filterResources();
+  async inputHtml() {
+    await this.#filterResources();
+    let resourceGrid = ``
     for (let i=0;i<this.#filteredList.length;i++) {
-      const resourceElement = document.getElementById('r'+i);
-      const resourceContent = `
+      resourceGrid += `
+        <div style="border:3px solid #003e7a;width:300px;height:250px"
         <div style="background-image:linear-gradient(rgba(0, 62, 122, 0.75),rgba(0, 62, 122, 0.75)),url(${this.#filteredList[i][2]});
           height:150px;
           width:100%;
@@ -15,17 +16,16 @@ class Resources {
           background-size:cover;
           background-position:center;"></div>
         <p style="font-size:14px;width:70%;margin:auto auto"><a href=${this.#filteredList[i][1]}>${this.#filteredList[i][0]}</a></p>
+        </div>
       `;
-      resourceElement.innerHTML = resourceContent;
-      resoruceElement.style.border = "3px solid #003e7a";
-      resourceElement.style.width = "300px";
-      resourceElement.style.height = "250px";
+      resourceGridElement = document.getElementById('js-resource-grid');
+      resourceGridElement.innerHTML = resourceGrid;
       
     }
   }
   
-  #filterResources() {
-    this.#getResources();
+  async #filterResources() {
+    await this.#getResources();
     this.#getSelectedOption();
 
     if (this.#filterOption==='all') {
@@ -36,11 +36,10 @@ class Resources {
     this.#filteredList = this.#rows.filter(row => row[3] === this.#filterOption);
   }
   
-  #getResources() {
-    const reader = new FileReader();
-    reader.onlodad = (e) => {
-      const text = e.target.results;
-      this.#rows = text.split('\n').map(row=>row.split(',')).slice(1); 
+  async #getResources() {
+    const response = await fetch('resource-data.csv')
+    const text = await response.text() 
+    this.#rows = text.split('\n').map(row=>row.split(',')).slice(1); 
     };
   }
   #getSelectedOption() {
