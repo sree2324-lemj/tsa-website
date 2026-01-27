2408f333eb2daff9a8764fd2e130d359ee6067a1
sree2324-lemj <palakodetisreecharan@gmail.com>
2026-01-27 14:14:58 -0500

    Update resources.js

--- resources.js
+++ resources.js
@@ -14,14 +14,16 @@ class Resources {
     let resourceGrid = ``
     for (let i=0;i<this.#filteredList.length;i++) {
       resourceGrid += `
-        <div style="border:3px solid #d57146;width:300px;height:250px">
+        <div style="border:6px solid #d57146;width:300px;height:250px; margin: 100px">
           <div style="background-image:linear-gradient(rgba(0, 62, 122, 0.75),rgba(0, 62, 122, 0.75)),url(${this.#filteredList[i][2]});
             height:150px;
             width:100%;
             background-blend-mode:multiply;
             background-size:cover;
             background-position:center;"></div>
-          <p style="font-size:14px;width:70%;margin:auto auto"><a href=${this.#filteredList[i][1]}>${this.#filteredList[i][0]}</a></p>
+          <div display:flex;align-items:center>
+            <p style="font-size:20px;width:70%;margin:auto auto;text-align:center;font-weight:bold;"><a href=${this.#filteredList[i][1]}>${this.#filteredList[i][0]}</a></p>
+          </div>
         </div>
       `;  
     }
