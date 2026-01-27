6eb1f8c568fa3bf2f08845fc0ecffde2a120c961
sree2324-lemj <palakodetisreecharan@gmail.com>
2026-01-27 12:23:22 -0500

    Update resources.js

--- resources.js
+++ resources.js
@@ -36,10 +36,9 @@ class Resources {
   }
   
   async #getResources() {
-    const response = await fetch('resource-data.csv')
-    const text = await response.text() 
+    const response = await fetch('resource-data.csv');
+    const text = await response.text();
     this.#rows = text.split('\n').map(row=>row.split(',')).slice(1); 
-    };
   }
   #getSelectedOption() {
     const selectElement = document.getElementById("js-resource-filter");
