3034718f0f17b49e04a803802e3d4cf5bccd3f31
sree2324-lemj <palakodetisreecharan@gmail.com>
2026-01-29 01:08:35 +0000

    add cors

--- backend.js
+++ backend.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-
+const cors = require('cors');
 const app = express();
 const PORT = 3000;
 
 // Middleware for plain text and static file serving
 app.use(express.text()); 
 app.use(express.static(__dirname));
+app.use(cors())
 
 
 app.post('/save', (req, res) => {
