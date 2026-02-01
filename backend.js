6722c04c4ad43ae1a4f7c161c45bd95df0b4cf14
sree2324-lemj <palakodetisreecharan@gmail.com>
2026-02-01 13:13:15 -0500

    Update backend.js

--- backend.js
+++ backend.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware for plain text and static file serving
 app.use(express.text()); 
