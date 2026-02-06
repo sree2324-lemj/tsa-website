cbacd1aea0296f6cc4aed1dec770a2ca62af4fa1
sree2324-lemj <palakodetisreecharan@gmail.com>
2026-02-06 15:04:04 -0500

    Update backend.js

--- backend.js
+++ backend.js
@@ -17,7 +17,7 @@ app.post('/feedback2', (req, res) => {
     const filePath = path.join(__dirname, 'feedback.txt');
     console.log(`feedback2 called and ${filePath} set`)
     // Appending string to file asynchronously
-    fs.appendFile(filePath, userFeedback + '\n', (err) => {
+    fs.writeFile(filePath, userFeedback + '\n', (err) => {
         if (err) return res.status(500).send('Error writing to file');
         res.send('Saved to feedback.txt');
     });
